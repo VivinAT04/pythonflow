b5729770483720b97a1815f3f767c1587d64c38b
Vivin Anitha Thambidurai <vivinanithathambidurai@gmail.com>
2026-06-19 01:51:02 +0100

    Display DAG graph in dashboard

--- pythonflow/dashboard.py
+++ pythonflow/dashboard.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from pythonflow.storage import Storage
 
 app = FastAPI()
 
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
+
 templates = Jinja2Templates(
     directory="templates"
 )
+
 
 @app.get("/")
 def dashboard(request: Request):
@@ -19,6 +27,7 @@ def dashboard(request: Request):
         request=request,
         name="index.html",
         context={
-            "rows": rows
+            "rows": rows,
+            "graph_path": "/static/dag_graph.png"
         }
     )
